c953f98de540f4a6e9dc21a818a4d4bd30ae9179
briansande <sandeb@oregonstate.edu>
2022-10-07 08:24:18 -0500

    MongoDB Updates

--- journal-rest/journal-controller.mjs
+++ journal-rest/journal-controller.mjs
@@ -1,10 +1,42 @@
 import 'dotenv/config';
 import express from 'express';
+import * as journal from './journal-model.mjs';
 
 
 const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
+
+// CREATE controller 
+app.post('/journal', async (req, res) => {
+    journal.createEntry(
+        req.body.title,
+        req.body.date,
+        req.body.author,
+        req.body.input,
+        req.body.output
+    ).then((entry) => {
+        res.status(201).json(entry);
+    }).catch((err) => {
+        res.status(400).json({ error: 'Creation of a document failed due to invalid syntax.' });
+    });
+});
+
+// GET entries controller
+app.get('/journal', async (req, res) => {
+    let filter = {};
+
+    journal.retrieveEntries(filter)
+        .then(entries => {
+            res.send(entries);
+        }).catch((err) => {
+            res.status(500).json({ error: '500:Connection to the server failed.' });
+        });
+});
+
+
+
+
 
 
 app.listen(PORT, () => {
---
+++ journal-rest/journal-model.mjs
@@ -0,0 +1,59 @@
+// Import dependencies.
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+// Connect based on the .env file parameters.
+mongoose.connect(
+    process.env.MONGODB_CONNECT_STRING,
+    { useNewUrlParser: true }
+);
+const db = mongoose.connection;
+
+// Confirm that the database has connected and print a message in the console.
+db.once("open", (err) => {
+    if (err) {
+        res.status(500).json({ error: '500:Connection to the server failed.' });
+    } else {
+        console.log('Successfully connected to MongoDB Movies collection using Mongoose.');
+    }
+});
+
+
+
+
+// SCHEMA: Define the collection's schema.
+const entrySchema = mongoose.Schema({
+    title: { type: String, required: true },
+    date: { type: Date, required: true },
+    author: { type: String, required: true },
+    input: { type: String, required: true },
+    output: { type: String, required: false }
+
+});
+// Compile the model from the schema.
+const Entry = mongoose.model("Entry", entrySchema);
+
+
+
+// CREATE entry *****************************************
+const createEntry = async (title, date, author, input, output) => {
+    const entry = new Entry({
+        title: title,
+        date: date,
+        author: author,
+        input: input,
+        output: output
+    });
+    return entry.save();
+}
+
+
+// RETRIEVE models *****************************************
+// Retrieve based on a filter and return a promise.
+const retrieveEntries = async (filter) => {
+    const query = Entry.find(filter);
+    return query.exec();
+}
+
+
+export { createEntry, retrieveEntries };
